b8e89d73471da15ae81fc3ffa18a1a3264e775e6
DavidCheah <tattwei46@gmail.com>
2019-07-10 22:29:31 +0800

    Added jdbc components

--- hackme/src/main/java/com/davidcheah/hackme/servlet/SearchServlet.java
+++ hackme/src/main/java/com/davidcheah/hackme/servlet/SearchServlet.java
@@ -2,6 +2,11 @@ package com.davidcheah.hackme.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Date;
 
 import javax.servlet.annotation.WebServlet;
@@ -11,12 +16,54 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/search")
 public class SearchServlet extends HttpServlet {
-	
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		System.out.println("GET request received " + request.getParameter("item"));
+		System.out.println("GET request received " + request.getParameter("bookName"));
+		search(request.getParameter("bookName"));
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		System.out.println("POST request received " + request.getParameter("item"));
+		System.out.println("POST request received " + request.getParameter("bookName"));
+	}
+
+	public void init() {
+		// 1. Load JDBC driver
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void search(String name) {
+		String url = "jdbc:mysql://localhost:3306/hackmedb";
+		String dbUsername = "root";
+		String dbPassword = "root";
+		String query = "select * from books where name='" + name + "'";
+
+		try {
+
+			// 2. Create a connection
+			Connection con = DriverManager.getConnection(url, dbUsername, dbPassword);
+
+			// 3. Create a statement
+			Statement st = con.createStatement();
+
+			// 4. Create a ResultSet
+			ResultSet rs = st.executeQuery(query);
+
+			while (rs.next()) {
+				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+			}
+
+			// 5. Close all connections
+			rs.close();
+			st.close();
+			con.close();
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
 	}
 }
---
+++ hackme/src/main/webapp/SearchPage.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Login JSP Page</title>
+</head>
+<body>
+	<form action="/hackme/search" method="get">
+		<h2> Search for products </h2> <input type="text" name="bookName" /> <br>
+		<input type="submit" />
+	</form>
+</body>
+</html>

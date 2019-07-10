c5d846d7301dd0a366440d33ddf2f7620bd16ae8
DavidCheah <tattwei46@gmail.com>
2019-07-10 23:46:09 +0800

    Added component to display result from db

--- hackme/src/main/java/com/davidcheah/hackme/servlet/SearchServlet.java
+++ hackme/src/main/java/com/davidcheah/hackme/servlet/SearchServlet.java
@@ -7,19 +7,26 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.davidcheah.hackme.model.Book;
+
 @WebServlet("/search")
 public class SearchServlet extends HttpServlet {
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		System.out.println("GET request received " + request.getParameter("bookName"));
-		search(request.getParameter("bookName"));
+		List<Book> bookList = search(request.getParameter("bookName"));
+		request.setAttribute("bookList", bookList);
+		request.getRequestDispatcher("/SearchPage.jsp").forward(request, response);
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -35,11 +42,14 @@ public class SearchServlet extends HttpServlet {
 		}
 	}
 
-	public void search(String name) {
+	public List<Book> search(String name) {
+		
 		String url = "jdbc:mysql://localhost:3306/hackmedb";
 		String dbUsername = "root";
 		String dbPassword = "root";
 		String query = "select * from books where name='" + name + "'";
+		
+		List<Book> bookList = new ArrayList<Book>();
 
 		try {
 
@@ -53,6 +63,8 @@ public class SearchServlet extends HttpServlet {
 			ResultSet rs = st.executeQuery(query);
 
 			while (rs.next()) {
+				Book book = new Book(rs.getInt(1), rs.getString(2), rs.getString(3));
+				bookList.add(book);
 				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
 			}
 
@@ -65,5 +77,7 @@ public class SearchServlet extends HttpServlet {
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
+		
+		return bookList;
 	}
 }
--- hackme/src/main/webapp/SearchPage.jsp
+++ hackme/src/main/webapp/SearchPage.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,8 +10,25 @@
 </head>
 <body>
 	<form action="/hackme/search" method="get">
-		<h2> Search for products </h2> <input type="text" name="bookName" /> <br>
-		<input type="submit" />
+		<h2>Search for products</h2>
+		<input type="text" name="bookName" /> <br> <input type="submit" />
 	</form>
+
+	<c:if test="${not empty bookList}">
+		<table border="1" style="width:100%">
+			<tr>
+				<td>Name</td>
+				<td>Author</td>
+			</tr>
+			<c:forEach items="${bookList}" var="book">
+				<tr>
+					<td>${book.name}</td>
+					<td>${book.author}</td>
+				</tr>
+			</c:forEach>
+		</table>
+	</c:if>
+
+
 </body>
 </html>

